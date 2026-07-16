0128f0c534aee78c90ff6159571af9ecf3f08f64
Alejandro Cueto <s3813718@students.fscj.edu>
2026-07-16 17:02:28 -0400

    Update PalmerPenguins.java

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,14 +1,24 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
+// Alejandro Cueto
+// 7/16/26
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 package edu.fscj.cop2800c.penguin;
 
 public class PalmerPenguins {
+
     public static void main(String[] args) {
+
         // Create an instance of PenguinAnalyzer
+        PenguinAnalyzer analyzer = new PenguinAnalyzer();
 
+        // Read CSV data
+        analyzer.readPenguinData();
 
+        // Print formatted output
+        analyzer.printPenguinData();
+
+        // Save results to a file
+        analyzer.writePenguinData();
     }
 }
